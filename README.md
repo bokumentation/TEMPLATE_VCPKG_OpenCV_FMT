fa79f4c02c94fa66fc7f04bd6afffb42ae4e6ac7
Ibe Ibrahim <ibraheli41@gmail.com>
2025-11-17 02:22:22 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Basic OpenCV FMT by using VCPKG packet manager
+
+I try to convert my python opencv, flask to CXX codebases. This is the basic project template.
+
+**Requirement:**
+- Package Manager: VCPKG
+- Compiler: MSVC 17 2022
+- Generator: Ninja 
+- Build Tools: CMake
+- Text Editor: VS Code
+    - Extension: CMake by Microsoft 
+- Intellisense: Clangd (optional but i use this)
---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv4/opencv2/videoio.hpp>
+#include <opencv2/core/utils/logger.hpp>
+
+#include <fmt/core.h>
+
+int main() {
+    cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_SILENT);
+    const int deviceID = 0; 
+    
+    cv::VideoCapture cap;
+    
+    cap.open(deviceID, cv::CAP_ANY); 
+
+    if (!cap.isOpened()) {
+        fmt::print(stderr, "ERROR: Could not open camera with device ID {}. Check camera connection and driver.\n", deviceID);
+        return 1;
+    }
+
+    fmt::print("Camera opened successfully (Device ID: {}). Press 'q' to exit.\n", deviceID);
+    
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+
+    cv::Mat frame;
+    int frameCount = 0;
+
+    while (true) {
+        cap >> frame; 
+
+        if (frame.empty()) {
+            fmt::print(stderr, "ERROR: Received empty frame. Exiting loop.\n");
+            break;
+        }
+        
+        frameCount++;
+        
+        cv::imshow("Live Video Feed (C++ OpenCV)", frame);
+        
+        if (cv::waitKey(1) == 'q') {
+            break;
+        }
+    }
+    
+    fmt::print("Exiting application. Total frames processed: {}.\n", frameCount);
+    cap.release();          
+    cv::destroyAllWindows(); 
+
+    return 0;
+}
